32e47c3f689993a951667748672a3819868cf943
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-06-06 10:06:45 +0200

    Add recyclerView

--- app/src/main/java/com/example/bottomnavigation/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/ui/fragments/HomeFragment.kt
@@ -2,10 +2,15 @@ package com.example.bottomnavigation.ui.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.bottomnavigation.R
+import com.example.bottomnavigation.ui.item.ChallengeInProgressItem
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
@@ -19,6 +24,21 @@ class HomeFragment : Fragment() {
         activity?.title = getString(R.string.title_home)
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.recyclerViewChallengeInProgress.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+
+        val itemAdapter = FastItemAdapter<ChallengeInProgressItem>()
+
+        for (i in 0..2) {
+            itemAdapter.add(ChallengeInProgressItem())
+        }
+
+        view.recyclerViewChallengeInProgress.adapter = itemAdapter
+
     }
 
 }
---
+++ app/src/main/java/com/example/bottomnavigation/ui/item/ChallengeInProgressItem.kt
@@ -0,0 +1,39 @@
+package com.example.bottomnavigation.ui.item
+
+import android.view.View
+import com.example.bottomnavigation.R
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
+import kotlinx.android.synthetic.main.card.view.*
+
+class ChallengeInProgressItem: AbstractItem<ChallengeInProgressItem, ChallengeInProgressItem.ChallengeInProgressViewHolder>() {
+    override fun getType(): Int {
+        return R.id.textViewChallengeInProgressTitle
+    }
+
+    override fun getViewHolder(v: View?): ChallengeInProgressViewHolder {
+        return ChallengeInProgressViewHolder(v)
+    }
+
+    override fun getLayoutRes(): Int {
+        return R.layout.card
+    }
+
+    class ChallengeInProgressViewHolder(itemView: View?): FastAdapter.ViewHolder<ChallengeInProgressItem>(itemView) {
+        override fun unbindView(item: ChallengeInProgressItem?) {
+            itemView.textViewChallengeInProgressTitle.text = null
+        }
+
+        override fun bindView(item: ChallengeInProgressItem?, payloads: MutableList<Any>?) {
+
+            // tu dois pouvoir récupérer l'objet de ton item
+            // ex : val challengeInProgress = item.challengeInProgress
+
+            // ex : itemView.textViewChallengeInProgressTitle.text = challengeInProgress.title
+
+            itemView.textViewChallengeInProgressTitle.text = "Challenge text"
+        }
+
+    }
+
+}

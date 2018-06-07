14e480a1db88f9598f866bde4deb982e12dab886
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-06-07 14:17:46 +0200

    Fix 'Invitations cards'

--- app/src/main/java/com/example/bottomnavigation/ui/item/ActualityItem.kt
+++ app/src/main/java/com/example/bottomnavigation/ui/item/ActualityItem.kt
@@ -6,32 +6,32 @@ import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.items.AbstractItem
 import kotlinx.android.synthetic.main.card_invitation.view.*
 
-class InvitationItem: AbstractItem<InvitationItem, InvitationItem.InvitationItemViewHolder>() {
+class ActualityItem: AbstractItem<ActualityItem, ActualityItem.ActualityItemViewHolder>() {
     override fun getType(): Int {
         return R.id.textViewChallengeInProgressTitle
     }
 
-    override fun getViewHolder(v: View?): InvitationItemViewHolder {
-        return InvitationItemViewHolder(v)
+    override fun getViewHolder(v: View?): ActualityItemViewHolder {
+        return ActualityItemViewHolder(v)
     }
 
     override fun getLayoutRes(): Int {
-        return R.layout.card_invitation
+        return R.layout.card_actuality
     }
 
-    class InvitationItemViewHolder(itemView: View?): FastAdapter.ViewHolder<InvitationItem>(itemView) {
-        override fun unbindView(item: InvitationItem?) {
+    class ActualityItemViewHolder(itemView: View?): FastAdapter.ViewHolder<ActualityItem>(itemView) {
+        override fun unbindView(item: ActualityItem?) {
             itemView.textViewActualityItemTitle.text = null
         }
 
-        override fun bindView(item: InvitationItem?, payloads: MutableList<Any>?) {
+        override fun bindView(item: ActualityItem?, payloads: MutableList<Any>?) {
 
             // tu dois pouvoir récupérer l'objet de ton item
             // ex : val challengeInProgress = item.challengeInProgress
 
             // ex : itemView.textViewChallengeInProgressTitle.text = challengeInProgress.title
 
-            itemView.textViewActualityItemTitle.text = "Lucille"
+            itemView.textViewActualityItemTitle.text = "Félicitation !"
         }
 
     }
